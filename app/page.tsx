fad97ff5641ada57a7b6871488048776c788551e
Isaac Brickner <91433002+isaacbrickner@users.noreply.github.com>
2025-04-02 17:57:27 -0700

    Update welcome message to include deployment information

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         height={150}
         priority
       />
-      <h1 className="text-4xl font-bold mt-4">Hello from Bricknei</h1>
+      <h1 className="text-4xl font-bold mt-4">Hello from Bricknei deployed with GH Actions.</h1>
     </div>
   );
 }
